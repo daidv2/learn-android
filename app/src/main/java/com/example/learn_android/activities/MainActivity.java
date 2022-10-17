ac90f6c60a8029a72ead891dba8ca31c331602fa
DaiDV <dangvandai1992@gmail.com>
2022-10-17 17:47:05 +0700

    Float Action Button; Snackbar

--- app/src/main/java/com/example/learn_android/activities/MainActivity.java
+++ app/src/main/java/com/example/learn_android/activities/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.learn_android.activities;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.learn_android.R;
 import com.example.learn_android.adapters.MyAdapter;
@@ -12,15 +17,33 @@ import com.example.learn_android.adapters.MyAdapter;
 /**
  * link references
  * https://www.javatpoint.com/android-tablayout
+ * https://viblo.asia/p/gioi-thieu-thu-vien-android-support-design-1qm6RW3qReJE
+ * https://xuanthulab.net/su-dung-snackbar-trong-lap-trinh-android.html
  */
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
+    ConstraintLayout mainContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainContent = findViewById(R.id.main_content);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(),"clicked on FAB", Toast.LENGTH_SHORT).show();
+                Snackbar.make(mainContent, "Hello, I'm Snackbar!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("DISMISS", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    }
+                }).show();
+            }
+        });
 
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
